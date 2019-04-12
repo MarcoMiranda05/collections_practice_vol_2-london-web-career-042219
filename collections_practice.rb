38c9154dc1a0169e3f79604336c1dcb4bf480aba
Marco Miranda <marmirand@hotmail.com>
2019-04-12 13:49:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def begins_with_r(x)
-  x.each do |member|
-    return false if member[0].downcase != "r"
+  x.each do |array|
+    return false if array[0].downcase != "r"
   end
   return true
 end
